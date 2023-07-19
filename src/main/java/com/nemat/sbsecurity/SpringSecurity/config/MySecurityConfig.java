e8f2981a5cb8628ebc5deac3f4b893587d9e01c7
Nemattullah <nemattullah.hussain@mindtree.com>
2023-07-19 10:26:38 +0530

    Custom UserDetailsService | Inmemory

--- src/main/java/com/nemat/sbsecurity/SpringSecurity/config/MySecurityConfig.java
+++ src/main/java/com/nemat/sbsecurity/SpringSecurity/config/MySecurityConfig.java
@@ -3,10 +3,32 @@ package com.nemat.sbsecurity.SpringSecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class MySecurityConfig {
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+//		InMemoryUserDetailsManager(C) is an implementation of UserDetailsService 
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+//		 Create a User (Inmemory)
+		UserDetails user = User.withUsername("tom")
+				.password(passwordEncoder().encode("cruise"))
+				.authorities("read").build();
+		userDetailsService.createUser(user);
+		return userDetailsService;
+	}
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
